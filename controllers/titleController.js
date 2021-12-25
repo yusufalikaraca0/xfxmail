1a90b233a57f77202ac4e35d65ca2f7fda5311e3
yusufalikaraca0 <kavunvesalatalik@gmail.com>
2021-12-25 20:25:47 +0300

    second

--- controllers/titleController.js
+++
@@ -1,8 +0,0 @@
-const titleControl = (req,res,next) =>{
-    if(typeof(res.locals.baslik) ===undefined){
-        res.locals.baslik = 'XfxMail';
-    }
-    return next();
-}
-
-module.exports = titleControl;
